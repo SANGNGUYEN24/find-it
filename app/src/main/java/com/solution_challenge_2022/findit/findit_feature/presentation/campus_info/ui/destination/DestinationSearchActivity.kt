6f84c6a07da44f69329f56bbf2161ae75bb1a4b2
Sang Nguyen <sangnguyendinh24work@gmail.com>
2022-03-18 20:53:12 +0700

    Retrieve current building location

---
+++ app/src/main/java/com/solution_challenge_2022/findit/findit_feature/presentation/campus_info/ui/destination/DestinationSearchActivity.kt
@@ -0,0 +1,11 @@
+package com.solution_challenge_2022.findit.findit_feature.presentation.campus_info.ui.destination
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DestinationSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destination_search)
+    }
+}
